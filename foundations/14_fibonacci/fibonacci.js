38a3acceb4bd0d2fb7344c2a4076e69a2027f900
odhiambovinogola <141261098+odhiambovinogola@users.noreply.github.com>
2026-07-18 11:49:35 +0300

    feat(14_fibonacci): add fibonacci calculation with string/negative handling

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (typeof n !== "number") n = parseInt(n);
 
+  if (n < 0) return "OOPS";
+  if (n === 0) return 0;
+
+  let previous = 0;
+  let current = 1;
+  for (let i = 2; i <= n; i++) {
+    const next = previous + current;
+    previous = current;
+    current = next;
+  }
+
+  return current;
 };
 
 // Do not edit below this line
